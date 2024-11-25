5a02edef0f51e6a30e5d1bbb00011c2728f53d52
All Mute ✓ <davidmerkulov@yandex.ru>
2024-11-25 03:29:49 +0300

    1st mdxses

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Руководства',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {to: '/showcase', label: 'Showcase', position: 'left'},
